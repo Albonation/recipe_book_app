118be5330ac1f15b4d18280e32f163ae0861e5b8
Alan Pruett <cpruett3@student.gsu.edu>
2026-03-03 20:40:21 -0500

    feat: configure app entry point and Material App

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(RecipeBookApp());
+}
+
+class RecipeBookApp extends StatelessWidget {
+  const RecipeBookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Book',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
